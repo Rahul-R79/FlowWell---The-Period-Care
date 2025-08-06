1c0476ab976c548dbf36e464a22daedcd0feb23b
Rahul-R79 <rahul.devworks@gmail.com>
2025-08-06 09:24:37 +0530

    validator middleware implemented

---
+++ server/middlewares/validators/authValidator.js
@@ -0,0 +1,46 @@
+import { body } from "express-validator";
+
+export const validateSignUp = [
+    body('name')
+    .notEmpty().withMessage('Name is Required').bail()
+    .matches(/^[A-Za-z]+$/).withMessage('Name must contain only letters')
+    .isLength({min: 3}).withMessage('Name must be at least 3 characters')
+    .trim(),
+
+    body('email')
+    .notEmpty().withMessage('Email is Required').bail()
+    .isEmail().withMessage('Enter a valid email')
+    .normalizeEmail(),
+
+    body('password')
+    .notEmpty().withMessage('Password is Required').bail()
+    .isLength({min: 6, max: 8}).withMessage('Password must be 6 to 8 characters long')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[0-9]/).withMessage('Password must contain at least one number')
+    .matches(/[@#$?_-]/).withMessage('Password must contain at least one special character'),
+
+    body('confirmPassword')
+    .notEmpty().withMessage('Confirm Password is Required').bail()
+    .custom((pass, {req})=>{
+        if(pass !== req.body.password){
+            throw new Error('Password is not Matching')
+        }
+        return true
+    })
+]
+
+export const validateSignIn = [
+    body('email')
+    .notEmpty().withMessage('Email is Required').bail()
+    .isEmail().withMessage('Enter a valid email')
+    .normalizeEmail(),
+
+    body('password')
+    .notEmpty().withMessage('Password is Required').bail()
+    .isLength({min: 6, max: 8}).withMessage('Password must be 6 to 8 characters long')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[0-9]/).withMessage('Password must contain at least one number')
+    .matches(/[@#$?_-]/).withMessage('Password must contain at least one special character'),
+]
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { SignIn, SignUp } from '../controllers/authController.js';
+import { validateSignIn, validateSignUp } from '../middlewares/validators/authValidator.js';
+import { handleValidation } from '../middlewares/validators/handleValidation.js';
 
 const router = express.Router();
 
-router.post('/signUp', SignUp);
-router.post('/signIn', SignIn);
+router.post('/signUp', SignUp, validateSignUp, handleValidation);
+router.post('/signIn', SignIn, validateSignIn, handleValidation);
 
 export default router;
