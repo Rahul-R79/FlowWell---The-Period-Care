e8830802a5d084287b65d11c171c335e76ae18da
Rahul-R79 <rahul.devworks@gmail.com>
2025-08-11 11:33:08 +0530

    amend commit fix

--- client/src/pages/user/profile/UserProfile.jsx
+++ client/src/pages/user/profile/UserProfile.jsx
@@ -27,31 +27,3 @@ function UserProfile(){
     )
 }
 
-export default UserProfile
-import { useSelector, useDispatch } from "react-redux"
-import { logoutUser } from "../../../features/auth/authSlice"
-import { useNavigate } from "react-router-dom";
-
-function UserProfile(){
-    const {user, loading} = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const handleLogout = async(e)=>{
-        e.preventDefault();
-        try{
-            await dispatch(logoutUser());
-            navigate('/');
-        }catch(err){
-            console.log('logout error', err);  
-        }
-    }
-    return(
-        <>
-        <h1>User Profile Page</h1>
-        <button onClick={handleLogout} disabled={loading}>Logout</button>
-        </>
-    )
-}
-
-export default UserProfile
