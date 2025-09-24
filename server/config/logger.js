23536632eec0611a05dddffc0a6048e01dda4b49
Rahul-R79 <rahul.devworks@gmail.com>
2025-09-24 10:43:29 +0530

    Add Winston logger

---
+++ server/config/logger.js
@@ -0,0 +1,27 @@
+//custom error logger
+import winston from "winston";
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level}]: ${message}`;
+});
+
+const logger = winston.createLogger({
+    level: "info",
+    format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
+    transports: [
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+    logger.add(
+        new winston.transports.Console({
+            format: combine(colorize(), timestamp(), logFormat),
+        })
+    );
+}
+
+export default logger;
