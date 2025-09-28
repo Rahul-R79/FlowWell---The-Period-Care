3e0fc4e179938ce5df874de11eb72dbc2806356f
Rahul-R79 <rahul.devworks@gmail.com>
2025-09-28 16:18:32 +0530

    fixed localhost cors issue

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import dotenv from 'dotenv';
-import { ConnectDB } from './config/db.js';
-import authRoutes from './routes/authRoutes.js'
-import adminRoutes from './routes/adminRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import passport from './config/passport.js';
-import notFound from './middlewares/pageNotFound.js';
-import morgan from 'morgan';
-import compression from 'compression';
-import helmet from 'helmet';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import { ConnectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import passport from "./config/passport.js";
+import notFound from "./middlewares/pageNotFound.js";
+import morgan from "morgan";
+import compression from "compression";
+import helmet from "helmet";
 
 dotenv.config();
 const app = express();
@@ -19,25 +19,28 @@ ConnectDB();
 
 app.use(helmet());
 app.use(compression());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-    origin: [
-        'https://www.flowwell.online',      
-        'https://flowwell.online'     
-    ],
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: [
+            "https://www.flowwell.online",
+            "https://flowwell.online",
+            "http://localhost:5173",
+        ],
+        credentials: true,
+    })
+);
 
 app.use(passport.initialize());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
 
 app.use(notFound);
 
